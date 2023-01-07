a3521c332f93a704aded01427ce84ec02c07329a
MKHLink <[likhonhasankomol@gmail.com]>
2023-01-07 14:34:56 -0500

    added search history, and able to click search history to see relevant weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,97 @@ var displayWeatherEl = document.getElementById("weather-display");
 var forecastContainerEl = document.querySelector("#forecast-container");
 var displayForecastEl=document.querySelector("#weather-forecast");
 var userBtn = document.getElementById("userBtn");
+var historyContainerEl = document.querySelector("previou-search");
 
+var hist1 =document.getElementById("userBtn0");
+var hist2=document.getElementById("userBtn1");
+var hist3=document.getElementById("userBtn2");
+var hist4=document.getElementById("userBtn3");
+var hist5=document.getElementById("userBtn4");
+
+//initializing the variable to empty array if no data in local storage present
+var seachHistory = (localStorage.seachHistory)?JSON.parse(localStorage.seachHistory):[];
+
+//adding event listners to the submit type buttons
 userBtn.addEventListener("click",getCity);
 
+hist1.addEventListener("click",getCity0);
+hist2.addEventListener("click",getCity1);
+hist3.addEventListener("click",getCity2);
+hist4.addEventListener("click",getCity3);
+hist5.addEventListener("click",getCity4);
+
+//populating the search history buttons from local storage
+ for(var i=0;i<5;i++){
+    document.getElementById("userBtn"+i).setAttribute("value",seachHistory[i]);
+ }
+
+//functions to get the city names and run the api call functions
 function getCity(){
     cityName = document.getElementById("userInput").value;
+
+    seachHistory.unshift(cityName);
+    localStorage.seachHistory = JSON.stringify(seachHistory);
+
+    getWeather(cityName);
+    getForecast(cityName); 
+}
+
+function getCity0(){
+    cityName = document.getElementById("userBtn0").value;
+
+    seachHistory.unshift(cityName);
+    localStorage.seachHistory = JSON.stringify(seachHistory);
+
+    getWeather(cityName);
+    getForecast(cityName); 
+}
+
+function getCity1(){
+    cityName = document.getElementById("userBtn1").value;
+
+    seachHistory.unshift(cityName);
+    localStorage.seachHistory = JSON.stringify(seachHistory);
+
+    getWeather(cityName);
+    getForecast(cityName); 
+}
+
+function getCity2(){
+    cityName = document.getElementById("userBtn2").value;
+
+    seachHistory.unshift(cityName);
+    localStorage.seachHistory = JSON.stringify(seachHistory);
+
+    getWeather(cityName);
+    getForecast(cityName); 
+}
+
+function getCity3(){
+    cityName = document.getElementById("userBtn3").value;
+
+    seachHistory.unshift(cityName);
+    localStorage.seachHistory = JSON.stringify(seachHistory);
+
+    getWeather(cityName);
+    getForecast(cityName); 
+}
+
+function getCity4(){
+    cityName = document.getElementById("userBtn4").value;
+
+    seachHistory.unshift(cityName);
+    localStorage.seachHistory = JSON.stringify(seachHistory);
 
     getWeather(cityName);
     getForecast(cityName); 
 }
 
 
+    
+
+
+//api call to get today's weather
 function getWeather(cityName)
 {
     var apiURL = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=imperial&APPID=2d2437f845de1bc0a6790ddf29c83740";
@@ -25,6 +105,7 @@ function getWeather(cityName)
     });
 }
 
+//api call to get forecast
 function getForecast(cityName)
 {
     var apiURL = "http://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&units=imperial&APPID=2d2437f845de1bc0a6790ddf29c83740";
@@ -36,7 +117,7 @@ function getForecast(cityName)
 
 }
 
-
+//displayes todays weather data
 function displayWeatherDdata(data)
 {
     var cityNameEl = data.name;
@@ -61,7 +142,7 @@ function displayWeatherDdata(data)
     
 }
 
-//forecast function
+//forecast weather data
 
 function displayForecastData(data)
 {   
