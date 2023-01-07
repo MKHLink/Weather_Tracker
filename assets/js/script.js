54129c5c3dca27e4c3ee8f3dc61de2471bf92969
MKHLink <[likhonhasankomol@gmail.com]>
2023-01-07 02:27:23 -0500

    used bootstrap to make UI look nicer and previos search result no longer presists upon new searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var weatherContainerEl = document.querySelector("#weather-container");
-var displayWeatherEl = document.querySelector("#weather-display");
+var displayWeatherEl = document.getElementById("weather-display");
 var forecastContainerEl = document.querySelector("#forecast-container");
 var displayForecastEl=document.querySelector("#weather-forecast");
 var userBtn = document.getElementById("userBtn");
@@ -8,7 +8,7 @@ userBtn.addEventListener("click",getCity);
 
 function getCity(){
     cityName = document.getElementById("userInput").value;
-    console.log(cityName);
+
     getWeather(cityName);
     getForecast(cityName); 
 }
@@ -19,7 +19,7 @@ function getWeather(cityName)
     var apiURL = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=imperial&APPID=2d2437f845de1bc0a6790ddf29c83740";
     fetch(apiURL).then(function(response){
         response.json().then(function(data){
-            console.log(data);
+
             displayWeatherDdata(data);
         });
     });
@@ -30,7 +30,6 @@ function getForecast(cityName)
     var apiURL = "http://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&units=imperial&APPID=2d2437f845de1bc0a6790ddf29c83740";
     fetch(apiURL).then(function(response){
         response.json().then(function(data){
-            console.log(data);
             displayForecastData(data);
         });
     });
@@ -43,27 +42,23 @@ function displayWeatherDdata(data)
     var cityNameEl = data.name;
     var dateEl =new Date(data.dt * 1000);
     var showDate = "("+(dateEl.getMonth()+1)+"/"+dateEl.getDate()+"/"+dateEl.getFullYear()+")";
-    console.log(showDate);
-    var nameEl = document.createElement("h2");
+   
+    var nameEl = document.getElementById("cityName");
     nameEl.textContent = cityNameEl+showDate;
 
     var currentTemp = data.main.temp;
-    var tempEl = document.createElement("div");
+    var tempEl = document.getElementById("temp");
     tempEl.textContent = "Temp: "+ currentTemp;
     
     var currentWind = data.wind.speed;
-    var windEl = document.createElement("div");
+    var windEl = document.getElementById("wind");
     windEl.textContent = "Wind Speed: "+currentWind + "MPH";
     
     var currentHumidity = data.main.humidity;
-    var humidityEl = document.createElement("div");
+    var humidityEl = document.getElementById("humidity");
     humidityEl.textContent = "Humidity: "+currentHumidity;
     
-    displayWeatherEl.appendChild(nameEl);
-    displayWeatherEl.appendChild(tempEl);
-    displayWeatherEl.appendChild(windEl);
-    displayWeatherEl.appendChild(humidityEl);
-    weatherContainerEl.appendChild(displayWeatherEl);
+    
 }
 
 //forecast function
@@ -74,26 +69,22 @@ function displayForecastData(data)
     {
         var dateEl =new Date(data.list[i].dt * 1000);
         var showDate = "("+(dateEl.getMonth()+1)+"/"+(dateEl.getDate()+1)+"/"+dateEl.getFullYear()+")";
-        var nameEl = document.createElement("h4");
+        var nameEl = document.getElementById("cityName"+i);
         nameEl.textContent = showDate;
 
         var currentTemp = data.list[i].main.temp;
-        var tempEl = document.createElement("div");
+        var tempEl = document.getElementById("temp"+i);
         tempEl.textContent = "Temp: "+ currentTemp;
     
         var currentWind = data.list[i].wind.speed;
-        var windEl = document.createElement("div");
+        var windEl = document.getElementById("wind"+i);
         windEl.textContent = "Wind Speed: "+currentWind + "MPH";
     
         var currentHumidity = data.list[i].main.humidity;
-        var humidityEl = document.createElement("div");
+        var humidityEl = document.getElementById("humidity"+i);
         humidityEl.textContent = "Humidity: "+currentHumidity;
     
-        displayForecastEl.appendChild(nameEl);
-        displayForecastEl.appendChild(tempEl);
-        displayForecastEl.appendChild(windEl);
-        displayForecastEl.appendChild(humidityEl);
-        forecastContainerEl.appendChild(displayForecastEl);
+        
     }
 }
 
